ff34c7823ea3f1211bde376ea4316cec8ba2e96b
kyle.macasinag <kylenmac@verizon.net>
2022-05-24 16:26:30 -0400

    Fix: corrected minor typo in entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,4 +4,4 @@ export DJANGO_SUPERUSER_EMAIL=admin@admin.com
 
 python manage.py migrate
 python manage.py createsuperuser --noinput
-python manage.py runserver 0.0.0.0;8000
+python manage.py runserver 0.0.0.0:8000
